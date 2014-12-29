5b8d4a24bb54a44f5f008bb1212d8b8288ddb8f6
ddoe <daniel.doeller@gameduell.de>
2014-12-29 11:13:14 +0100

    - fixed loading behaviour for assets with relative path

--- backends/filesystem_flash/filesystem/FileSystem.hx
+++ backends/filesystem_flash/filesystem/FileSystem.hx
@@ -1,5 +1,8 @@
 package filesystem;
 
+import flash.Lib;
+import flash.events.IOErrorEvent;
+import flash.events.Event;
 import types.Data;
 
 import flash.net.URLLoader;
@@ -7,27 +10,47 @@ import flash.net.URLRequest;
 import flash.net.URLLoaderDataFormat;
 import flash.system.LoaderContext;
 
-import flash.events.*;
 import flash.utils.ByteArray;
 
 using StringTools;
 
 class FileSystem
 {
+    public var staticData: Map<String, Data>;
+    public var cachedData: Map<String, Data>;
+    public var tempData: Map<String, Data>;
 
-    public var staticData : Map<String, Data>;
-    public var cachedData : Map<String, Data>;
-    public var tempData : Map<String, Data>;
-    private function new() : Void
+    private static var requestsLeft: Int;
+    private static var fileSystemInstance: FileSystem;
+
+    private var staticDataURL: String = "static://";
+    private var cachedDataURL: String = "cached://";
+    private var tempDataURL: String = "temp://";
+
+    public static inline function instance(): FileSystem
+    {
+        return fileSystemInstance;
+    }
+
+    public static function initialize(finishedCallback: Void -> Void): Void
+    {
+        if (fileSystemInstance == null)
+        {
+            fileSystemInstance = new FileSystem();
+        }
+        preloadStaticAssets(finishedCallback);
+    }
+
+    private function new(): Void
     {
         cachedData = new Map<String, Data>();
         tempData = new Map<String, Data>();
         staticData = new Map<String, Data>();
     }
 
-    public function getFileList(url : String, ?recursive : Bool = true) : Array<String>
+    public function getFileList(url: String, ?recursive: Bool = true): Array<String>
     {
-        if(url.startsWith(staticDataURL))
+        if (url.startsWith(staticDataURL))
         {
             var withoutPrefix = trimURLPrefix(url);
 
@@ -39,241 +62,264 @@ class FileSystem
         }
     }
 
-    private function getStaticFileList(url : String, recursive : Bool) : Array<String>
+    private function getStaticFileList(url: String, recursive: Bool): Array<String>
     {
         var filteredFiles = [];
-        for(file in staticData.keys())
+        for (file in staticData.keys())
         {
-            if(file.startsWith(url))
+            if (file.startsWith(url))
             {
-                if(recursive || file.substr(url.length).indexOf("/") == -1)
+                if (recursive || file.substr(url.length).indexOf("/") == -1)
+                {
                     filteredFiles.push(staticDataURL + file);
+                }
             }
         }
 
         return filteredFiles;
     }
 
-    private var staticDataURL : String = "static://";
-    public function urlToStaticData() : String
+    public function urlToStaticData(): String
     {
         return staticDataURL;
     }
 
-    private var cachedDataURL : String = "cached://";
-    public function urlToCachedData() : String
+    public function urlToCachedData(): String
     {
         return cachedDataURL;
     }
 
-    private var tempDataURL : String = "temp://";
-    public function urlToTempData() : String
+    public function urlToTempData(): String
     {
         return tempDataURL;
     }
 
-
-
-    public function createFile(url : String) : Bool
+    public function createFile(url: String): Bool
     {
-        if(url.startsWith(staticDataURL))
+        if (url.startsWith(staticDataURL))
+        {
             return false;
+        }
 
         var map = getDataDictionaryBasedOnPrefix(url);
 
-        if(map == null)
+        if (map == null)
+        {
             return false;
+        }
 
         var withoutPrefix = trimURLPrefix(url);
 
-        if(map.exists(withoutPrefix))
+        if (map.exists(withoutPrefix))
+        {
             return false;
+        }
 
         map[withoutPrefix] = new Data(0);
         return true;
     }
 
-    public function getFileWriter(url : String) : FileWriter
+    public function getFileWriter(url: String): FileWriter
     {
-        if(url.startsWith(staticDataURL))
+        if (url.startsWith(staticDataURL))
+        {
             return null;
+        }
 
         var map = getDataDictionaryBasedOnPrefix(url);
 
-        if(map == null)
+        if (map == null)
+        {
             return null;
+        }
 
         var withoutPrefix = trimURLPrefix(url);
-        if(!map.exists(withoutPrefix))
+        if (!map.exists(withoutPrefix))
+        {
             return null;
+        }
 
         var data = map[withoutPrefix];
-        if(data == null) /// folder
+        if (data == null) /// folder
+        {
             return null;
+        }
 
         return new FileWriter(data);
     }
 
-    public function getFileReader(url : String) : FileReader
+    public function getFileReader(url: String): FileReader
     {
         var withoutPrefix = trimURLPrefix(url);
         var map = getDataDictionaryBasedOnPrefix(url);
 
-        if(map == null)
+        if (map == null)
+        {
             return null;
+        }
 
-        if(!map.exists(withoutPrefix))
+        if (!map.exists(withoutPrefix))
+        {
             return null;
+        }
 
         var data = map[withoutPrefix];
-        if(data == null) /// folder
+        if (data == null) /// folder
+        {
             return null;
+        }
 
         return new FileReader(data);
     }
 
-    public function createFolder(url : String) : Bool
+    public function createFolder(url: String): Bool
     {
-        if(url.startsWith(staticDataURL))
+        if (url.startsWith(staticDataURL))
+        {
             return false;
+        }
 
         var map = getDataDictionaryBasedOnPrefix(url);
 
-        if(map == null)
+        if (map == null)
+        {
             return false;
+        }
 
         var withoutPrefix = trimURLPrefix(url);
 
-        if(map.exists(withoutPrefix))
+        if (map.exists(withoutPrefix))
+        {
             return false;
+        }
 
         map[withoutPrefix] = null;
         return true;
     }
 
-    public function deleteFile(url : String) : Void
+    public function deleteFile(url: String): Void
     {
         var map = getDataDictionaryBasedOnPrefix(url);
 
-        if(map == null)
+        if (map == null)
+        {
             return;
+        }
 
         var withoutPrefix = trimURLPrefix(url);
         map.remove(withoutPrefix);
     }
 
-    public function deleteFolder(url : String) : Void
+    public function deleteFolder(url: String): Void
     {
 
         var map = getDataDictionaryBasedOnPrefix(url);
 
-        if(map == null)
+        if (map == null)
+        {
             return;
+        }
 
         var withoutPrefix = trimURLPrefix(url);
 
         var toRemove = [];
-        for(key in map.keys())
+        for (key in map.keys())
         {
-            if(key.startsWith(withoutPrefix))
+            if (key.startsWith(withoutPrefix))
+            {
                 toRemove.push(key);
+            }
         }
 
-        for(key in toRemove)
+        for (key in toRemove)
         {
             map.remove(key);
         }
 
     }
 
-    public function urlExists(url : String) : Bool
+    public function urlExists(url: String): Bool
     {
         var map = getDataDictionaryBasedOnPrefix(url);
 
-        if(map == null)
+        if (map == null)
+        {
             return false;
+        }
 
         var withoutPrefix = trimURLPrefix(url);
         return map.exists(withoutPrefix);
     }
 
-    public function isFolder(url : String) : Bool
+    public function isFolder(url: String): Bool
     {
         var map = getDataDictionaryBasedOnPrefix(url);
 
-        if(map == null)
+        if (map == null)
+        {
             return false;
+        }
 
         var withoutPrefix = trimURLPrefix(url);
         return map.exists(withoutPrefix) && map[withoutPrefix] == null;
     }
 
-    public function isFile(url : String) : Bool
+    public function isFile(url: String): Bool
     {
         var map = getDataDictionaryBasedOnPrefix(url);
 
-        if(map == null)
+        if (map == null)
+        {
             return false;
+        }
 
         var withoutPrefix = trimURLPrefix(url);
         return map.exists(withoutPrefix) && map[withoutPrefix] != null;
     }
 
-    public function getFileSize(url : String) : Int
+    public function getFileSize(url: String): Int
     {
         var map = getDataDictionaryBasedOnPrefix(url);
 
-        if(map == null)
+        if (map == null)
+        {
             return 0;
+        }
 
         var withoutPrefix = trimURLPrefix(url);
 
         if (!map.exists(withoutPrefix))
+        {
             return 0;
+        }
 
         return map[withoutPrefix].offsetLength;
     }
 
-/// SINGLETON
-
-    static var fileSystemInstance : FileSystem;
-    static public inline function instance() : FileSystem
+    public static function preloadStaticAssets(complete: Void -> Void): Void
     {
-        return fileSystemInstance;
-    }
-
-    public static function initialize(finishedCallback : Void -> Void):Void
-    {
-        if(fileSystemInstance == null)
-        {
-            fileSystemInstance = new FileSystem();
-        }
-        preloadStaticAssets(finishedCallback);
-    }
-    static private var requestsLeft : Int;
-    public static function preloadStaticAssets(complete : Void -> Void) : Void
-    {
-        if(filesystem.StaticAssetList.list.length == 0)
+        if (filesystem.StaticAssetList.list.length == 0)
         {
             complete();
             return;
         }
-        function encodeURLElement(element:String) : String
+        function encodeURLElement(element: String): String
         {
             return element.urlEncode();
         }
         requestsLeft = filesystem.StaticAssetList.list.length;
 
-        for(val in filesystem.StaticAssetList.list)
+        for (val in filesystem.StaticAssetList.list)
         {
-            var valWithAssets = "assets/"+val;
+            var valWithAssets = "assets/" + val;
             valWithAssets.split("/").map(encodeURLElement).join("/");
-            var loader:URLLoader = new URLLoader();
+            var loader: URLLoader = new URLLoader();
             loader.dataFormat = URLLoaderDataFormat.BINARY;
-            var loaderContext:LoaderContext = new LoaderContext();
+            var loaderContext: LoaderContext = new LoaderContext();
 
-            loader.addEventListener(Event.COMPLETE, function(event:Event):Void{
-                var bytes:ByteArray = event.target.data;
+            loader.addEventListener(Event.COMPLETE, function(event: Event): Void
+            {
+                var bytes: ByteArray = event.target.data;
                 var data = new Data(0);
                 data.byteArray = bytes;
 
@@ -282,43 +328,57 @@ class FileSystem
                 FileSystem.instance().staticData[correctedUrl] = data;
 
                 requestsLeft--;
-                if(requestsLeft == 0)
+                if (requestsLeft == 0)
                 {
                     complete();
                 }
             });
 
-            loader.addEventListener(IOErrorEvent.IO_ERROR, function(event:IOErrorEvent):Void{
+            loader.addEventListener(IOErrorEvent.IO_ERROR, function(event: IOErrorEvent): Void
+            {
                 trace(event);
             });
-
-            loader.load(new URLRequest(valWithAssets));
+            var req = new URLRequest(getBaseURL() + valWithAssets);
+            loader.load(req);
         }
     }
 
-/// HELPERS
+    /// HELPERS
 
-    private function trimURLPrefix(url : String) : String
+    /**
+    * If using relative URLs - the base URL of the root SWF is needed for loading assets properly
+    */
+    private static function getBaseURL(): String
+    {
+        var splitter = Lib.current.loaderInfo.url.split("/");
+        splitter.pop();
+        return splitter.join("/") + "/";
+    }
+
+    private function trimURLPrefix(url: String): String
     {
         var withoutPrefix = url.substr(url.indexOf(":") + 1);
-        while(withoutPrefix.startsWith("/"))
+
+        while (withoutPrefix.startsWith("/"))
+        {
             withoutPrefix = withoutPrefix.substr(1);
+        }
         return withoutPrefix;
     }
 
-    private function getDataDictionaryBasedOnPrefix(url : String) : Map<String, Data>
+    private function getDataDictionaryBasedOnPrefix(url: String): Map<String, Data>
     {
         var withoutPrefix = trimURLPrefix(url);
 
-        if(url.startsWith(staticDataURL))
+        if (url.startsWith(staticDataURL))
         {
             return staticData;
         }
-        else if(url.startsWith(cachedDataURL))
+        else if (url.startsWith(cachedDataURL))
         {
             return cachedData;
         }
-        else if(url.startsWith(tempDataURL))
+        else if (url.startsWith(tempDataURL))
         {
             return tempData;
         }
