5c06275fa58ecdadc9e4ea4d2fdfb7dda784e200
Rui Campos <rui.campos@gameduell.de>
2015-02-27 19:01:45 +0100

    update ci scripts with the new tool

--- ci.sh
+++ ci.sh
@@ -5,115 +5,14 @@ cd tests
 
 rm -rf Export
 
-expect -c "
-spawn haxelib run duell build android -test -verbose -D jenkins
+haxelib run duell update -verbose -yestoall
 
-set timeout -1
+haxelib run duell build android -test -verbose -D jenkins -yestoall
 
-expect {
-	\"is not currently installed.\" {
+haxelib run duell build html5 -test -verbose -D jenkins -yestoall
 
-		send \"y\r\"
+haxelib run duell build flash -test -verbose -D jenkins -yestoall
 
-		exp_continue
-	}
-	\"is missing,\" {
-
-		send \"y\r\"
-
-		exp_continue
-	}
-	\"not up to date\" {
-
-		send \"y\r\"
-
-		exp_continue
-	}
-}
-"
-
-
-
-expect -c "
-spawn haxelib run duell build html5 -test -verbose -D jenkins
-
-set timeout -1
-
-expect {
-	\"is not currently installed.\" {
-
-		send \"y\r\"
-
-		exp_continue
-	}
-	\"is missing,\" {
-
-		send \"y\r\"
-
-		exp_continue
-	}
-	\"not up to date\" {
-
-		send \"y\r\"
-
-		exp_continue
-	}
-}
-"
-
-
-expect -c "
-spawn haxelib run duell build flash -test -verbose -D jenkins
-
-set timeout -1
-
-expect {
-	\"is not currently installed.\" {
-
-		send \"y\r\"
-
-		exp_continue
-	}
-	\"is missing,\" {
-
-		send \"y\r\"
-
-		exp_continue
-	}
-	\"not up to date\" {
-
-		send \"y\r\"
-
-		exp_continue
-	}
-}
-"
-
-expect -c "
-spawn haxelib run duell build ios -test -verbose -D jenkins
-
-set timeout -1
-
-expect {
-	\"is not currently installed.\" {
-
-		send \"y\r\"
-
-		exp_continue
-	}
-	\"is missing,\" {
-
-		send \"y\r\"
-
-		exp_continue
-	}
-	\"not up to date\" {
-
-		send \"y\r\"
-
-		exp_continue
-	}
-}
-"
+haxelib run duell build ios -test -verbose -D jenkins -yestoall
 
 
