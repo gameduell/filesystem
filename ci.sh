e149e61ef25a4f8a4c73a1edfbab723e0a23560e
jxav <joao.xavier@gameduell.de>
2015-03-18 16:26:15 +0100

    Revert "Test killing adb server before building android"
    This reverts commit dca840deb2f6cb99f52fa536a40c041382cc5508.

--- ci.sh
+++ ci.sh
@@ -7,8 +7,6 @@ rm -rf Export
 
 haxelib run duell update -verbose -yestoall
 
-$ANDROID_HOME/platform-tools/adb kill-server
-
 haxelib run duell build android -test -verbose -D jenkins -yestoall
 
 haxelib run duell build html5 -test -verbose -D jenkins -yestoall
